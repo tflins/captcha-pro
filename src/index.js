f4f5088725632a8bc79d7da7cd62b07e3e49029f
tflin <tflins@163.com>
2020-09-13 17:00:15 +0800

    add: 绘制背景

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-(function(window) {
+;(function (window) {
   'use strict'
 
   function optionsVerify(options) {
@@ -16,15 +16,65 @@
     }
   }
 
+  function isHtmlElement(node) {
+    return node && node.nodeType === Node.ELEMENT_NODE
+  }
+
+  function getRandomInt(min, max) {
+    return ~~(Math.random() * (max - min + 1) + min)
+  }
+
+  function getRandomColor(min, max) {
+    const r = getRandomInt(min, max)
+    const g = getRandomInt(min, max)
+    const b = getRandomInt(min, max)
+
+    return `rgba(${r}, ${g}, ${b})`
+  }
+
   class Captcha {
     constructor(options = {}) {
       optionsVerify(options)
 
+      const defaultOptions = {
+        width: 100,
+        height: 30
+      }
+
+      options = Object.assign(defaultOptions, options)
+
+      this.el = options.el
       this.content = options.content
       this.width = options.width
       this.height = options.height
       this.fontSize = options.fontSize
+
+      this.init()
     }
+
+    init() {
+      if (isHtmlElement(this.el)) this.$el = this.el
+      else this.$el = document.querySelector(this.el)
+
+      this.$canvas = document.createElement('canvas')
+      this.ctx = this.$canvas.getContext('2d')
+      this.draw()
+      this.$el.appendChild(this.$canvas)
+    }
+
+    draw() {
+      this.ctx.fillStyle = getRandomColor(200, 220)
+      this.ctx.fillRect(0, 0, this.width, this.height)
+    }
+
+    drawCanvas() {}
+
+    drawContent() {}
+
+    drawLine() {}
+
+    drawDot() {}
   }
 
-}(window))
+  window.Captcha = Captcha
+})(window)
